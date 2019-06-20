883d7c977abe3e5e13625a8ba7e6a9d93761b9e4
njaara <49847985+njaara@users.noreply.github.com>
2019-06-20 07:39:11 -0400

    Add files via upload

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -18,9 +18,9 @@ $(function() {
         /* Loops through each feed in the allFeeds object and ensures
          * it has a URL defined and that the URL is not empty.
          */
-         it('URLs are defined, not empty', function() {
+         it('URLs are defined/not empty', function() {
            for(var i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].url).toBeDefined();             // https://devhints.io/jasmine
+             expect(allFeeds[i].url).toBeDefined();                     // https://devhints.io/jasmine
              expect(allFeeds[i].url.length).not.toBe(0);
            }
          });
@@ -28,7 +28,7 @@ $(function() {
         /* Loops through each feed in the allFeeds object and ensures
          * it has a name defined and that the name is not empty.
          */
-         it('Names are defined, not empty', function() {
+         it('Names are defined/not empty', function() {
            for(var i = 0; i < allFeeds.length; i++) {
            expect(allFeeds[i].name).toBeDefined();
            expect(allFeeds[i].name.length).not.toBe(0);
@@ -40,22 +40,22 @@ $(function() {
 
       // Ensures the menu element is hidden by default
       it('menu is hidden', function() {
-        expect($('body').hasClass('menu-hidden')).toBeTruthy();                // https://api.jquery.com/hasclass/
+        expect($('body').hasClass('menu-hidden')).toBeTruthy();               // https://api.jquery.com/hasclass/
       });
 
       // Ensures the menu changes visibility when the menu icon is clicked.
       it('menu visible on click', function() {                              // http://api.jquery.com/trigger/
         $('.menu-icon-link').trigger('click');
-        expect($('body').hasClass('menu-hidden')).toBe(false);
+        expect($('body').hasClass('menu-hidden')).toBeFalsy();
 
         $('.menu-icon-link').trigger('click');
-        expect($('body').hasClass('menu-hidden')).toBe(true);
+        expect($('body').hasClass('menu-hidden')).toBeTruthy();
 
       });
   });
 
     describe('Initial Entries', function() {
-      beforeEach(function(done) {                 // https://jasmine.github.io/tutorials/async
+      beforeEach(function(done) {                            // https://jasmine.github.io/tutorials/async
         loadFeed(0, function() {
           done();
         });
@@ -63,29 +63,30 @@ $(function() {
 
       // feed container has at least one entry element
       it('at least one entry in feed', function () {
-        expect($('.feed .entry').length).not.toBe(0);      // https://jasmine.github.io/api/3.4/matchers
+        expect($('.feed .entry').length).toBeGreaterThan(0);      // https://jasmine.github.io/api/3.4/matchers
       });
   });
 
     describe('New Feed Selection', function() {
-      ogFeed = $('.feed').html();                     // https://www.w3schools.com/jquery/html_html.asp
+      ogFeed = $('.feed').html();                           // https://www.w3schools.com/jquery/html_html.asp
+
       beforeEach(function(done) {
         loadFeed(0, function() {
         });
         loadFeed(1, function() {
+          newFeed = $('.feed').html();
           done();
         });
       });
 
       // newFeed and ogFeed are not the same
       it('new feed loaded', function() {
-        newFeed = $('.feed').html();
         expect(newFeed).not.toBe(ogFeed);
       });
     });
 }());
 
-/* Also referenced: 
+/* Also referenced:
 * https://medium.com/letsboot/testing-javascript-with-jasmine-basics-48efe03cf973
 * https://www.youtube.com/watch?v=pPt4oOKNdEk
 *
